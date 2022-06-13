1265ae53fae83a4f4b05eb371f71d97419e9c9f6
Atanas Dragolov <adragolov@gmail.com>
2022-06-13 09:16:43 +0300

    Adding exception tests

---
+++ tests/Saorsa.QueryEngine.Tests/QueryEngineExceptionTests.cs
@@ -0,0 +1,47 @@
+namespace Saorsa.QueryEngine.Tests;
+
+public class QueryEngineExceptionTests
+{
+    [Test]
+    public void TestConstructorWithKeyOnly()
+    {
+        var val = new Random().Next();
+        var exception = new QueryEngineException(val);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(exception.ErrorCode, Is.EqualTo(exception.Key));
+            Assert.That(exception.ErrorCode, Is.EqualTo(val));
+            Assert.That(exception.InnerException, Is.Null);
+        });
+    }
+
+    [Test]
+    public void TestConstructorWithMessage()
+    {
+        var val = new Random().Next();
+        var msg = Guid.NewGuid().ToString("N");
+        var exception = new QueryEngineException(val, msg);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(exception.Message, Is.EqualTo(msg));
+            Assert.That(exception.InnerException, Is.Null);
+        });
+    }
+
+    [Test]
+    public void TestConstructorWithInnerException()
+    {
+        var val = new Random().Next();
+        var msg = Guid.NewGuid().ToString("N");
+        var exception = new QueryEngineException(val, msg, new ArgumentNullException());
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(exception.Message, Is.EqualTo(msg));
+            Assert.That(exception.InnerException, Is.Not.Null);
+            Assert.That(exception.InnerException is ArgumentNullException);
+        });
+    }
+}
